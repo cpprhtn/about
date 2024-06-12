83c28ed1e6c7976062ccad54058e698071e4d9ef
Coalery <doralife12@gmail.com>
2024-06-13 01:11:56 +0900

    feat: 깃허브 및 링크드인 링크 추가

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
@@ -15,6 +17,22 @@ export default function Header() {
           보이지 않는 곳에서 동작하는 것들에 대해 알아보는 걸 좋아합니다.
           <br />
           최근에는 Nest.js의 동작과 구현을 많이 살펴보고 있습니다.
+        </p>
+      </div>
+      <div>
+        <p className="text-lg">
+          <Link
+            className="underline underline-offset-4 text-black hover:text-black/60"
+            href="https://github.com/Coalery"
+          >
+            Github
+          </Link>
+          <Link
+            className="underline underline-offset-4 text-black hover:text-black/60 ml-4"
+            href="https://linkedin.com/in/Coalery"
+          >
+            LinkedIn
+          </Link>
         </p>
       </div>
     </header>
