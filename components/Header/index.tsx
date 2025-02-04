26091682bcf7f9695e893d45f7e45c65e21ea8c7
Junwon Lee <cpprhtn@naver.com>
2025-02-04 13:31:02 +0900

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Header() {
-  const years = new Date().getFullYear() - 2013 + 1;
+  // const years = new Date().getFullYear() - 2013 + 1;
 
   return (
     <header>
@@ -11,12 +11,12 @@ export default function Header() {
       </div>
       <div>
         <p className="text-lg">
-          {years}년째 꾸준히 개발하고 있습니다.
+          {/* {years}년째 꾸준히 개발하고 있습니다. */}
           <br />
           데이터 분석 및 AI를 공부하고 있습니다.
         </p>
         <p className="text-lg">
-        제 장점은 오랜 기간 다양한 개발 분야를 경험하며 쌓은 폭넓은 기술적 이해와 문제 해결 능력입니다.
+        제 장점은 다양한 개발 분야를 경험하며 쌓은 폭넓은 기술적 이해와 문제 해결 능력입니다.
         또한 여러 개발 커뮤니티에서 활발히 활동하며 의사소통, 업무 분담, 우선순위 조정 등의 협업 역량을 키워왔습니다.
         새로운 환경에서도 빠르게 적응하고, 팀이 효과적으로 목표를 달성할 수 있도록 기여할 자신이 있습니다.
         </p>
